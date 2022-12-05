f9f813c4ca3f5bea3d58285bb4c5a341a2dfc265
ryosantouchh <kittitat.suntimak@gmail.com>
2022-12-06 02:19:29 +0700

    finish all features and catch any case

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState();
+  const [result, setResult] = useState();
+  const [resultColor, setResultColor] = useState();
+
+  const palindrome = (str, event) => {
+    let nonWord = /[\W_]/g;
+    let lowerCaseStr = str.toLowerCase().replace(nonWord, "");
+    let reverseStr = lowerCaseStr.split("").reverse().join("");
+
+    if (lowerCaseStr === reverseStr) {
+      const isPalin = `It is palindrome`;
+      setResultColor(true);
+      setResult(isPalin);
+    } else {
+      const isNotPalin = `It is not palindrome`;
+      setResultColor(false);
+      setResult(isNotPalin);
+    }
+
+    return lowerCaseStr === reverseStr;
+  };
+
+  const handleCheckButton = (event) => {
+    palindrome(event.target.value);
+    event.preventDefault();
+  };
+
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+
+    if (event.target.value === "") {
+      setResult("");
+    }
+  };
+
+  const handleClickReset = (event) => {
+    setInput("");
+    setResult("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="window-box">
+      <div className="app-box">
+        <div className="palindrome-box">
+          <div className="palindrome-head-text">Palindrome Checker</div>
+          <form>
+            <input
+              className="palindrome-input"
+              type="text"
+              placeholder="Enter text"
+              value={input}
+              onChange={handleInputChange}
+            />
+            <div>
+              <button
+                type="submit"
+                className="btn-check"
+                onClick={handleCheckButton}
+                disabled={!input}
+              >
+                Check
+              </button>
+              <button className="btn-check" onClick={handleClickReset}>
+                Reset
+              </button>
+            </div>
+          </form>
+          <div className="center-box">
+            <div
+              className="result-box"
+              style={resultColor ? { color: "green" } : { color: "red" }}
+            >
+              {result}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
